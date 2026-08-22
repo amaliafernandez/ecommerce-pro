4142a08cccdaf2de358440f711b3287cc1df3d60
Stefan Dios Mayarin <116821916+stefanmdev@users.noreply.github.com>
2026-08-22 20:58:23 -0300

    fix: corregí referencia al contexto en useAppContext

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext } from "react";
 export const AppContext = createContext(undefined);
 
 export function useAppContext() {
-  const context = useContext(useAppContext);
+  const context = useContext(AppContext);
   if (!context) {
     throw new Error("useAppContext debe usarse dentro de un AppProvider");
   }
